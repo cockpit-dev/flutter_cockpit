88ccc0edf9397c3d886c43e3930aeea4d11758d8
iota9star <iota9star@foxmail.com>
2026-05-21 18:46:07 +0800

    chore(demo): refresh drift codegen dependencies

--- examples/cockpit_demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/cockpit_demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import flutter_cockpit
-import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterCockpitPlugin.register(with: registry.registrar(forPlugin: "FlutterCockpitPlugin"))
-  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
